681873f3fb4cd930c39877cc540c0abde9642e73
ricsirogi <rikike888@gmail.com>
2024-03-05 12:49:08 +0100

    Added a bunch of words and updated a py script
    The py script now also has the ability to write in allDecks.json

--- py_scripts/print_decks.py
+++ py_scripts/print_decks.py
@@ -1,10 +1,13 @@
 """
 This program prints out all the decks in the decks folder 
 in a way that I can just copy and paste it into menu.js
+
+also it can automatically write the decks to allDecks.json
 """
 
 import glob
 import os
+import json
 
 current_dir = os.path.dirname(__file__)
 root_dir = os.path.dirname(current_dir)
@@ -15,4 +18,18 @@ txt_files = glob.glob(decks_dir + "/*.txt")
 # Get only the filenames
 filenames = [os.path.basename(txt)[0:-4] for txt in txt_files]
 
-print(filenames)
+ans = input("separately or all together? (s/a)")
+
+if ans.lower() == "s":
+    for filename in filenames:
+        print(filename)
+elif ans.lower() == "a":
+    print(filenames)
+
+ans = input("write to allDecks.json? (y/n)")
+
+if ans == "y":
+    with open(os.path.join(decks_dir, "allDecks.json"), "w") as f:
+        f.write("")
+        json.dump({"decks": filenames}, f)
+    print("allDecks.json written")
